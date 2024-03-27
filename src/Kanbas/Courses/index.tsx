f3b607b368174e65adbb161e9fef452a602b3410
Monroe Chung <monroe.chung@gmail.com>
2024-03-26 21:11:33 -0400

    fix console error

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Navigate, Route, Routes, useLocation, useParams } from "react-router-dom";
 import { HiMiniBars3, HiOutlineArrowRight } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
