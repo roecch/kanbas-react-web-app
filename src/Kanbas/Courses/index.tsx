13760ff7eb8aa7b2b9cb56c1a401aa0a9369fc71
Monroe Chung <monroe.chung@gmail.com>
2024-03-26 21:08:17 -0400

    fix console error

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -8,9 +8,12 @@ import Assignments from "./Assignments";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+
 function Courses() {
   const { courseId } = useParams();
-  const COURSES_API = "http://localhost:4000/api/courses";
+  const COURSES_API = `${API_BASE}/api/courses`;
   const [course, setCourse] = useState<any>({ _id: "" });
   const courIDstr = courseId?.toString()!
   const location = useLocation();
